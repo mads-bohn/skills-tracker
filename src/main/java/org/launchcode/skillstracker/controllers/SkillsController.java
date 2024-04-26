9cd302ec3a03f9ee49f4ff1da6a5f2480c5bfe88
mads-bohn <mady.bohn@gmail.com>
2024-04-25 23:01:26 -0500

    completed studio

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -1,23 +1,22 @@
 package org.launchcode.skillstracker.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
 public class SkillsController {
 
-    @GetMapping("/")
-    public static String createList() {
+    @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
+    public static String createList(@RequestParam String username, @RequestParam String firstfav, @RequestParam String secondfav, @RequestParam String thirdfav) {
         return "<html>" +
                 "<body>\n" +
                 "    <h1>Skills Tracker</h1>\n" +
-                "    <h2>My Favorite Languages</h2>\n" +
+                "    <h2>" + username + "'s Favorite Languages</h2>\n" +
                 "    <ol>\n" +
-                "        <li>Java</li>\n" +
-                "        <li>JavaScript</li>\n" +
-                "        <li>HTML</li>\n" +
+                "        <li>" +firstfav+ "</li>\n" +
+                "        <li>" +secondfav+ "</li>\n" +
+                "        <li>" +thirdfav+ "</li>\n" +
                 "    </ol>\n" +
                 "</body>" +
                 "</html>";
@@ -27,35 +26,37 @@ public class SkillsController {
     public static String createForm() {
         return "<html>" +
                 "<body>\n" +
-                "    <form action='form' method='post'>\n" +
+                "    <form action='/' method='post'>\n" +
                 "       <label for=\"username\">Name:</label>\n" +
                 "        <input type=\"text\" name=\"username\" id=\"username\">" +
+                "<br/>" +
                 "        <label for=\"firstfav\">First favorite language:</label>\n" +
                 "        <select name=\"firstfav\" id=\"firstfav\">\n" +
-                "            <option value=\"java\">Java</option>\n" +
-                "            <option value=\"js\">Javascript</option>\n" +
-                "            <option value=\"html\">HTML</option>\n" +
-                "            <option value=\"ruby\">Ruby</option>\n" +
-                "            <option value=\"python\">Python</option>\n" +
+                "            <option value=\"Java\">Java</option>\n" +
+                "            <option value=\"Javascript\">Javascript</option>\n" +
+                "            <option value=\"HTML\">HTML</option>\n" +
+                "            <option value=\"Ruby\">Ruby</option>\n" +
+                "            <option value=\"Python\">Python</option>\n" +
                 "        </select>\n" +
                 "<br/>" +
                 "        <label for=\"secondfav\">Second favorite language:</label>\n" +
                 "        <select name=\"secondfav\" id=\"secondfav\">\n" +
-                "            <option value=\"java\">Java</option>\n" +
-                "            <option value=\"js\">Javascript</option>\n" +
-                "            <option value=\"html\">HTML</option>\n" +
-                "            <option value=\"ruby\">Ruby</option>\n" +
-                "            <option value=\"python\">Python</option>\n" +
+                "            <option value=\"Java\">Java</option>\n" +
+                "            <option value=\"Javascript\">Javascript</option>\n" +
+                "            <option value=\"HTML\">HTML</option>\n" +
+                "            <option value=\"Ruby\">Ruby</option>\n" +
+                "            <option value=\"Python\">Python</option>\n" +
                 "        </select>\n" +
                 "<br/>" +
                 "        <label for=\"thirdfav\">Third favorite language:</label>\n" +
                 "        <select name=\"thirdfav\" id=\"thirdfav\">\n" +
-                "            <option value=\"java\">Java</option>\n" +
-                "            <option value=\"js\">Javascript</option>\n" +
-                "            <option value=\"html\">HTML</option>\n" +
-                "            <option value=\"ruby\">Ruby</option>\n" +
-                "            <option value=\"python\">Python</option>\n" +
+                "            <option value=\"Java\">Java</option>\n" +
+                "            <option value=\"Javascript\">Javascript</option>\n" +
+                "            <option value=\"HTML\">HTML</option>\n" +
+                "            <option value=\"Ruby\">Ruby</option>\n" +
+                "            <option value=\"Python\">Python</option>\n" +
                 "        </select>\n" +
+                "<br/>" +
                 "<input type=\"submit\">" +
                 "    </form>\n" +
                 "</body>" +
